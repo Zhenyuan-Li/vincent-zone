46976ecb645b4baf3405569b8dcd3c402ff81511
Vincent0Lee <vincent0lee95@gmail.com>
2022-01-27 16:38:00 +0800

    Fetch .md from file system

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,37 @@
 import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostData, getPostFiles } from '../../lib/post-util';
 
 // human readable search engine friendly routes: slug
-function PostDetailPage() {
-  return <PostContent />;
+function PostDetailPage(props) {
+  const { post } = props;
+
+  return <PostContent post={post} />;
 }
 
 export default PostDetailPage;
+
+export const getStaticProps = async (ctx) => {
+  const { params } = ctx;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+};
+
+export const getStaticPaths = async () => {
+  const postFileNames = getPostFiles();
+
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ''));
+
+  return {
+    // since not all of posts are need to pre-rendered
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false,
+  };
+};
