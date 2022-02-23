0ea9ee2606ca76f026769a2d28543609f144d2af
Vincent0Lee <vincent0lee95@gmail.com>
2022-02-23 20:48:52 +0800

    Up to deployment

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,10 +1,15 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { coldarkDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
+
+SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('css', css);
 
 function PostContent(props) {
   const { post } = props;
@@ -34,7 +39,7 @@ function PostContent(props) {
       const { className, children } = code;
       const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
       return (
-        <SyntaxHighlighter style={coldarkDark} language={language}>
+        <SyntaxHighlighter style={atomDark} language={language}>
           {children}
         </SyntaxHighlighter>
       );
